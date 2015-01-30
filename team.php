cf20ec4c7dd673c1fa87602c3c9dd5dd152be069
SneakyKoala <jasperdre@gmail.com>
2015-01-30 16:59:59 +0100

    fixed pathfinding

---
+++ team.php
@@ -0,0 +1,13 @@
+<div class="contact-holder">
+  <div class="contact-image-holder">
+    <img src="http://www.gravatar.com/avatar/12e7b1887a9310b9d6643c5530e1dcc8.png" />
+  </div>
+  <hr />
+  <div class="content">
+    <b>Programmer</b> <p>MLG pro 360 no scope programming php is love php is life.
+  </div>
+  <hr />
+  <div class="bottom">
+    <p>Jasper Drescher</p>
+  </div>  
+</div>
