2a1e77e22a7e168bef47412bee54c8759d2e330d
JDreau <jasperdre@gmail.com>
2015-03-10 19:02:05 +0100

    Removed input
    JS not server-side, need php but not supported in github

--- grid.js
+++ grid.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-   $('.grid').hide().fadeIn(4000);
-   $('button').click(function() {
-    	var toAdd = $("input[name=addgrid]").val();
-        $('.grid-container').append("<li class='grid'>"+toAdd+"</li>");
+$(document).ready(function() {
+$('.grid').hide().fadeIn(4000);
     });
-});
