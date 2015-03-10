ef4deb9ec87a0a2484f3c7f43eb7d54cefff89e3
JDreau <jasperdre@gmail.com>
2015-03-10 08:44:11 +0100

    test fluid grid form input

--- grid.js
+++ grid.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
-   $('.grid').hide().fadeIn(4000);    
+   $('.grid').hide().fadeIn(4000);
+   $('button').click(function() {
+    	var toAdd = $("input[name=addgrid]").val();
+        $('.grid-container').append("<li class='grid'>"+toAdd+"</li>");
+    });
 });
